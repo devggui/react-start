4602282f3ea482fa83e9577e44803fa14f4b65c1
Guilherme Henrique <devguiet@gmail.com>
2022-02-28 13:38:37 -0300

    Finished Video

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,42 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import { Tweet } from "./components/Tweet"
+import { AppRoutes } from "./Routes"
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <AppRoutes />
+  );
+
+  // const [tweets, setTweets] = useState<string[]>([
+  //   "Tweet 1",
+  //   "Tweet 2",
+  //   "Tweet 3",
+  //   "Tweet 4"
+  // ])
+
+  // function createTweet() {
+  //   setTweets([...tweets, 'Tweet 5'])
+  // }
+  
+  // return (
+  //   <div>
+  //     {tweets.map(tweet => {
+  //       return <Tweet text={tweet}/>
+  //     })}
+
+  //     <button 
+  //       onClick={createTweet}
+  //       style={{
+  //         backgroundColor: '#8257e6',
+  //         border: 1,
+  //         padding: '60px 12px',
+  //         color: '#FFF',
+  //       }}
+  //     >
+  //       Adicionar Tweet</button>
+  //   </div>
+  // );
 }
 
 export default App
